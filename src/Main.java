d971d3810e939f2250905ac90b6928b0f20ca738
Anastasia Minikes <minikes.a@mail.ru>
2023-05-05 20:07:12 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 28354;
+        int mile = 20;
+        int bonus = ticket/mile;
+
+        System.out.println(bonus+" бонусных миль/мили");
+
+
+
+    }
+}
