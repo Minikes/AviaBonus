ed4681ddcc7a15a8ddee0d6e154107730a1f2b8b
Anastasia Minikes <minikes.a@mail.ru>
2023-05-09 22:20:36 +0700

    function

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,13 +1,9 @@
 public class BonusMilesService {
-    public int calculate(int price){
-        int miles;
-        if (price>20) {
-            miles = price/20;} else {
-            miles = 1;}
-    return miles;
+    public int calculate(int price) {
+        int miles = price / 20;
+        return miles;
     }
-
-    }
+}
 
 
 
